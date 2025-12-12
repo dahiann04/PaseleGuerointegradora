d412ca9ca6f4bbee83102923dbeb47fa979ee00f
Juan Pablo Roldan Andrade <20243ds139@utez.edu.mx>
2025-12-12 04:16:12 -0600

    Agregué la clase AuctionRepository y la referencia al ApiService

---
+++ PaseleGuero/app/src/main/java/com/utez/paseleguero/ui/repository/AuctionRepository.kt
@@ -0,0 +1,12 @@
+package com.utez.paseleguero.ui.repository
+
+import com.utez.paseleguero.ui.model.Bid
+import com.utez.paseleguero.ui.model.Product
+import com.utez.paseleguero.ui.network.ApiConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class AuctionRepository {
+
+    private val api = ApiConfig.apiService
+}
