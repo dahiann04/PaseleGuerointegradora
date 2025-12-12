f8f2ca3597fbb478d375ff2f0efd3eaf8e5c1318
dahiann04 <20243ds121@utez.edu.mx>
2025-12-12 02:56:23 -0600

    Agregué el título de login y el espacio debajo

---
+++ PaseleGuero/app/src/main/java/com/utez/paseleguero/ui/viewmodel/LoginScreen.kt
@@ -0,0 +1,57 @@
+package com.utez.paseleguero.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.utez.paseleguero.R
+import com.utez.paseleguero.ui.viewmodel.LoginViewModel
+
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    viewModel: LoginViewModel = viewModel()
+) {
+    val username by viewModel.username
+    val password by viewModel.password
+    val isLoading by viewModel.isLoading
+    val loginError by viewModel.loginError
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "Pásele Güero Logo",
+            modifier = Modifier
+                .padding(0.dp)
+                .width(420.dp)
+                .height(320.dp)
+        )
+        Text(
+            text = "Inicio de sesión",
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
